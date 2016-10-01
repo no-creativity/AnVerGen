fe247f2013f4fff6ecefbc15f5e492105bd6740d
yanqd0 <yanqd0@icloud.com>
2016-10-01 22:02:40 +0800

    Add Git.groovy and its test.

---
+++ src/main/groovy/org/no_creativity/anvergen/Git.groovy
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2016 Yan QiDong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.no_creativity.anvergen
+
+/**
+ * This is a subset wrapper of Git.
+ * <p>
+ * There may be another way to do so, but it makes this repository much bigger.
+ * See:
+ * <ul>
+ * <li><a href="https://github.com/ethomson/jagged">jagged</a>
+ * Java bindings to libgit2.</li>
+ * <li><a href="https://github.com/libgit2/libgit2">libgit2</a>
+ * A C-lang implementation of the Git core methods.</li>
+ * </ul>
+ *
+ * @author yanqd0
+ */
+public class Git {
+    /**
+     * This is the return of {@link #getLatestTag()} if there is no git tag yet.
+     */
+    public final static String DEFAULT_TAG = "0.0"
+    private static def tag = DEFAULT_TAG
+
+    private Git() {
+        throw new UnsupportedOperationException("This class should never be instantiated!")
+    }
+
+    /**
+     * This method calculates the number of commit(s) in the specific list.
+     * If <code>from</code> is <code>null</code>, the list is the whole git history.
+     *
+     * @param from The start of the commit list to be count. The default is <code>null</code>.
+     * @param to The end of the commit list to be count. The default is "HEAD".
+     * @return The commit count between <code>from</code> and <code>to</code>.
+     */
+    public static int calculateCommitCount(String from = null, String to = "HEAD") {
+        String cmd
+        if (from == null || from.trim().isEmpty()) {
+            cmd = "git rev-list --count $to"
+        } else {
+            cmd = "git rev-list --count $from...$to"
+        }
+
+        def process = cmd.execute()
+        process.waitFor()
+        return process.getText().toInteger()
+    }
+
+    /**
+     * If there is no git tag yet, then this returns {@link #DEFAULT_TAG}.
+     *
+     * @return The latest git tag.
+     * @see #DEFAULT_TAG
+     */
+    public static String getLatestTag() {
+        if (!DEFAULT_TAG.equals(tag)) {
+            return tag
+        }
+
+        def cmd = "git for-each-ref --sort=-taggerdate --count=1 --format %(tag) refs/tags"
+        def process = cmd.execute()
+        process.waitFor()
+        tag = process.getText().trim()
+
+        if (tag.isEmpty()) {
+            tag = DEFAULT_TAG
+        }
+
+        return tag
+    }
+
+    /**
+     * It's the same as the end of <code>git describe --always</code>.
+     *
+     * @return The short SHA1 as the commit description.
+     */
+    public static String getShortSha1() {
+        def process = "git rev-parse HEAD".execute()
+        process.waitFor()
+        return process.getText().substring(0, 7)
+    }
+}
---
+++ src/test/groovy/org/no_creativity/anvergen/GitTest.groovy
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2016 Yan QiDong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.no_creativity.anvergen
+
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertNotNull
+
+/**
+ * @author yanqd0
+ */
+public class GitTest {
+    /**
+     * @see Git#calculateCommitCount()
+     */
+    @Test
+    public void getCommitCount0() throws Exception {
+        def process = "git rev-list --count HEAD".execute()
+        process.waitFor()
+        def count = process.getText().toInteger()
+        assertEquals(count, Git.calculateCommitCount())
+    }
+
+    /**
+     * @see Git#calculateCommitCount(String)
+     */
+    @Test
+    public void getCommitCount1() throws Exception {
+        assertEquals(Git.calculateCommitCount(), Git.calculateCommitCount(""))
+        assertEquals(0, Git.calculateCommitCount("HEAD"))
+        assertEquals(1, Git.calculateCommitCount("HEAD^"))
+    }
+
+    /**
+     * @see Git#calculateCommitCount(String, String)
+     */
+    @Test
+    public void getCommitCount2() throws Exception {
+        assertEquals(2, Git.calculateCommitCount("HEAD~3", "HEAD^"))
+    }
+
+    @Test
+    public void getLastTag() throws Exception {
+        def tag = Git.getLatestTag()
+        assertNotNull(tag)
+        assertFalse(tag.trim().isEmpty())
+        assertEquals(tag, Git.getLatestTag())
+
+        def cmd = "git for-each-ref --sort=-taggerdate --count=1 --format %(tag) refs/tags"
+        def process = cmd.execute()
+        process.waitFor()
+        def t = process.getText().trim()
+        if (t.isEmpty()) {
+            assertEquals(Git.DEFAULT_TAG, tag)
+        } else {
+            assertEquals(t, tag)
+        }
+    }
+
+    @Test
+    public void getShortSha1() throws Exception {
+        def process = "git describe --always".execute()
+        process.waitFor()
+        def description = process.getText()
+        def length = description.length()
+        def shortSha1 = description.substring(length - 8, length).trim()
+        assertEquals(shortSha1, Git.getShortSha1())
+    }
+}
